070b2303114b6d74e1c461d9d533fe645f7ae07c
Zac Hooper <zachooper@Zacs-MBP.modem>
2022-04-04 11:02:31 +0930

    Added link to hosted CV

--- components/cvButton.js
+++ components/cvButton.js
@@ -4,7 +4,7 @@ const CVButton = ({ children }) => {
 
     return (
         <div className="submit-btn">
-            <a href={getStrapiURL("/uploads/Zac_Hooper_Resume_9b067f5214.pdf?updated_at=2022-03-31T21:21:09.939Z")} className="main-btn" target="_blank" rel="noreferrer">
+            <a href="https://res.cloudinary.com/zachooper/image/upload/v1649035695/Zac_Hooper_Resume_ba3fd554e9.pdf?updated_at=2022-04-04T01:28:16.197Z" className="main-btn" target="_blank" rel="noreferrer">
                 <span className="btn-icon"><i className="fas fa-download"></i></span>
                 <span className="btn-text">Download CV</span>
             </a>
